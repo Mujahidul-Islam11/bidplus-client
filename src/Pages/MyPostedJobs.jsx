41fb3aee04d743bae09f7ebef9d93677f7dd7728
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-11-07 00:00:46 -0800

    I have completed the deleting method in my posted jobs route

--- src/Pages/MyPostedJobs.jsx
+++ src/Pages/MyPostedJobs.jsx
@@ -1,27 +1,12 @@
-import { NavLink, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import PostedJobDetails from "./PostedJobDetails";
 
 const MyPostedJobs = () => {
   const postedJobs = useLoaderData();
+  
   return (
     <div className="grid md:grid-cols-4 container mx-auto gap-4">
-      {postedJobs.map((posted) => (
-        <>
-          <div className="card bg-base-100 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">{posted.jobTitle}</h2>
-              <p>{posted.price}</p>
-              <p>{posted.description}</p>
-              <p>{posted.deadline}</p>
-              <div className="card-actions justify-between ">
-                <NavLink to={`/updateDetails/${posted._id}`}>
-                  <button className="btn btn-primary">Update</button>
-                </NavLink>
-                <button className="btn btn-primary">Delete</button>
-              </div>
-            </div>
-          </div>
-        </>
-      ))}
+      {postedJobs.map(posted => <PostedJobDetails posted={posted} key={posted._id}></PostedJobDetails>)}
     </div>
   );
 };
---
+++ src/Pages/PostedJobDetails.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import { NavLink } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const PostedJobDetails = ({ posted }) => {
+  const handleDeleteJob = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/JobsId/${posted._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire(
+                "Deleted!",
+                "Your job data has been deleted successfully",
+                "success"
+              );
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">{posted.jobTitle}</h2>
+        <p>{posted.price}</p>
+        <p>{posted.description}</p>
+        <p>{posted.deadline}</p>
+        <div className="card-actions justify-between ">
+          <NavLink to={`/updateDetails/${posted._id}`}>
+            <button className="btn btn-primary">Update</button>
+          </NavLink>
+          <button onClick={handleDeleteJob} className="btn btn-primary">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostedJobDetails;
