d6843ebb804cd6d237683821dd4247cd453a3e8c
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-07-04 23:10:38 +0600

    Displayes testimonials json data and created a testimonial card to show data

--- src/Pages/Testimonials.jsx
+++ src/Pages/Testimonials.jsx
@@ -17,7 +17,14 @@ const Testimonials = () => {
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {
             testimonials?.map(testimonial => <SwiperSlide>
-                <h3>{testimonial?.name}</h3>
+                <div className="w-3/4 mx-auto space-y-6 border px-8 py-4 rounded-md shadow-md">
+                    <img className="w-20 h-20 object-cover rounded-full" src={testimonial?.image} alt="" />
+                <p className="text-2xl">{testimonial?.testimonial}</p>
+                <div>
+                <h3 className="text-xl font-semibold mb-1">{testimonial?.name}</h3>
+                <p className="text-gray-500">{testimonial?.role}</p>
+                </div>
+                </div>
             </SwiperSlide>)
         }
       </Swiper>
